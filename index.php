291c85ab4e15c44a77dc6adad6d9e46299528759
Mario <marioosuna26@gmail.com>
2021-03-06 15:46:41 +0100

    TODO

---
+++ index.php
@@ -0,0 +1,331 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/style.css">
+
+  <title>Mario Osuna Ordóñez</title>
+</head>
+
+<body>
+
+
+  <div class="container">
+    <div id="resume-header" class="row">
+      <div class="col-3">
+        <img id="profile-picture" src="img/logo.png" alt="">
+      </div>
+      <div class="col">
+
+        <?php echo '<h1>Mario Osuna Ordóñez</h1><br>';
+
+        echo 'Desarrollador web';
+
+        echo "
+        <ul>
+          <li><a href='mailto:marioosuna26@gmal.com'>Mail</a></li>          
+          <li><a href='https://www.linkedin.com/in/mario-osuna-ordóñez'>Linkedin</a></li>
+          <li><a href='https://github.com/MarioOsuna'>GitHub</a></li>
+        </ul>";
+        ?>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <h2 class="border-bottom-gray">PHP.Progamación</h2>
+        <p>Ejercicios y prácticas de php del primer y segundo trimestre</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div>
+          <h3 class="border-bottom-gray">SEGUNDO TRIMESTRE</h3>
+          <ul>
+            <li class="work-position">
+              <h5>Bases de datos&nbsp;&nbsp;&nbsp;<a href="https://github.com/MarioOsuna/2DAW_DWES/tree/main/2_Trimestre"><img class="logo" src="img/github.png" alt=""></a></h5>
+
+
+              <ul>
+                <li><a href="2_Trimestre/BDD/index.php">Superheroes</a></li>
+                <li><a href="2_Trimestre/POO/mascotas/index.php">Animales</a></li>
+                <li><a href="2_Trimestre/POO/app/index.php">Alumnos</a></li>
+
+              </ul>
+            </li>
+
+          </ul>
+        </div>
+        <div>
+          <h3 class="border-bottom-gray">PRÁCTICAS</h3>
+          <div class="project">
+            <h5>Teatro</h5>
+            <div class="row">
+              <div class="col-3">
+                <img id="profile-picture" src="img/teatro.jpg" alt="">
+              </div>
+              <div class="col">
+                <p>Página sobre una base de datos de un teatro</p>
+                <strong><a href="2_Trimestre/Examen2021OsunaOrdóñezMario/index.php">Gran teatro</a></strong>
+                <span class="badge badge-secondary">PHP</span>
+                <span class="badge badge-secondary">HTML</span>
+                <span class="badge badge-secondary">CSS</span>
+                <a href="https://github.com/MarioOsuna/2DAW_DWES/tree/main/2_Trimestre/Examen2021OsunaOrd%C3%B3%C3%B1ezMario"><img class="logo" src="img/github.png" alt=""></a>
+              </div>
+            </div>
+          </div>
+          <div class="project">
+            <h5>Symblog (mdvc)</h5>
+            <div class="row">
+              <div class="col-3">
+                <img id="profile-picture" src="img/symblog.jpeg" alt="">
+              </div>
+              <div class="col">
+                <p>Blog que utiliza el MDVC</p>
+                <strong><a href="2_Trimestre/symblog/public/index.php">Symblog</a></strong>
+                <span class="badge badge-secondary">PHP</span>
+                <span class="badge badge-secondary">HTML</span>
+                <span class="badge badge-secondary">CSS</span>
+                <a href="https://github.com/MarioOsuna/2DAW_DWES/tree/main/2_Trimestre/symblog"><img class="logo" src="img/github.png" alt=""></a>
+              </div>
+            </div>
+          </div>
+          <div class="project">
+            <h5>Jobeet (symfony)</h5>
+            <div class="row">
+              <div class="col-3">
+                <img id="profile-picture" src="img/jobeet.gif" alt="">
+              </div>
+              <div class="col">
+                <p>Jobeet, práctica con symfony para visualizar ofertas de trabajo</p>
+                <strong><a href="1_Trimestre/Formularios/Pract/Verbos.php">Jobeet</a></strong>
+                <span class="badge badge-secondary">PHP</span>
+                <span class="badge badge-secondary">SYMFONY</span>
+                <span class="badge badge-secondary">HTML</span>
+                <span class="badge badge-secondary">CSS</span>
+                <a href="https://github.com/MarioOsuna/2DAW_DWES/tree/main/2_Trimestre/symfony/jobeet"><img class="logo" src="img/github.png" alt=""></a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div>
+          <h3 class="border-bottom-gray">PRIMER TRIMESTRE</h3>
+          <ul>
+          <li class="work-position">
+              <h5>Autentificación&nbsp;&nbsp;&nbsp;<a href="https://github.com/MarioOsuna/2DAW_DWES/tree/main/1_Trimestre/Autentificacion"><img class="logo" src="img/github.png" alt=""></a></h5>
+
+
+              <ul>
+                <li><a href="1_Trimestre/Autentificacion/autentificacionbasica/index.php">Autentificación Básica</a></li>
+                <li><a href="1_Trimestre/Autentificacion/Ejercicio3/index.php">Ejercicio 2</a></li>
+                
+
+              </ul>
+            </li>
+            <li class="work-position">
+              <h5>Sesiones&nbsp;&nbsp;&nbsp;<a href="https://github.com/MarioOsuna/2DAW_DWES/tree/main/1_Trimestre/Sesiones"><img class="logo" src="img/github.png" alt=""></a></h5>
+
+
+              <ul>
+                <li><a href="1_Trimestre/Sesiones/ejercicio1.php">Ejercicio 1</a></li>
+                <li><a href="1_Trimestre/Sesiones/ejercicio2.php">Ejercicio 2</a></li>
+                <li><a href="1_Trimestre/Sesiones/ejercicio3.php">Ejercicio 3</a></li>
+                <li><a href="1_Trimestre/Sesiones/ejercicio4.php">Ejercicio 4</a></li>
+
+              </ul>
+            </li>
+            <li class="work-position">
+              <h5>Cookies&nbsp;&nbsp;&nbsp;<a href="https://github.com/MarioOsuna/2DAW_DWES/tree/main/1_Trimestre/Cookies"><img class="logo" src="img/github.png" alt=""></a></h5>
+
+              <ul>
+                <li><a href="1_Trimestre/Cookies/1.php">Ejercicio 1</a></li>
+                <li><a href="1_Trimestre/Cookies/2.php">Ejercicio 2</a></li>
+                <li><a href="1_Trimestre/Cookies/3.php">Ejercicio 3</a></li>
+                <li><a href="1_Trimestre/Cookies/4.php">Ejercicio 4</a></li>
+
+              </ul>
+            </li>
+            <li class="work-position">
+              <h5>Funciones&nbsp;&nbsp;&nbsp;<a href="https://github.com/MarioOsuna/2DAW_DWES/tree/main/1_Trimestre/Funciones"><img class="logo" src="img/github.png" alt=""></a></h5>
+
+              <ul>
+                <li><a href="1_Trimestre/Funciones/Ejemplo1.php">Ejemplo 1</a></li>
+                <li><a href="1_Trimestre/Funciones/Ejemplo2.php">Ejemplo 2</a></li>
+                <li><a href="1_Trimestre/Funciones/Ejercicio1.php">Ejercicio 1</a></li>
+                <li><a href="1_Trimestre/Funciones/Ejercicio2.php">Ejercicio 2</a></li>
+                <li><a href="1_Trimestre/Funciones/Ejercicio3.php">Ejercicio 3</a></li>
+                <li><a href="1_Trimestre/Funciones/Ejercicio4.php">Ejercicio 4</a></li>
+                <li><a href="1_Trimestre/Funciones/Ejercicio5.php">Ejercicio 5</a></li>
+              </ul>
+            </li>
+            
+            <li class="work-position">
+              <h5>Formularios&nbsp;&nbsp;&nbsp;<a href="https://github.com/MarioOsuna/2DAW_DWES/tree/main/1_Trimestre/Formularios"><img class="logo" src="img/github.png" alt=""></a></h5>
+
+              <ul>
+                <li><a href="1_Trimestre/Formularios/form1.php">Ejemplo 1</a></li>
+                <li><a href="1_Trimestre/Formularios/form2.php">Ejemplo 2</a></li>
+                <li><a href="1_Trimestre/Formularios/form3.php">Ejemplo 3</a></li>
+                <li><a href="1_Trimestre/Ficheros/CargarFotos/form.php">Subir imagen</a></li>
+                <li><a href="1_Trimestre/Formularios/ValidacionFormulario.php">ValidaciónFomulario</a></li>
+              </ul>
+
+            </li>
+            <li class="work-position">
+              <h5>Arrays&nbsp;&nbsp;&nbsp;<a href="https://github.com/MarioOsuna/2DAW_DWES/blob/main/1_Trimestre/Arrays/EjemploArrayComunidades.php"><img class="logo" src="img/github.png" alt=""></a></h5>
+              <ul>
+                <li><a href="1_Trimestre/Arrays/EjemploArrayComunidades.php">ArrayComunidades</a></li>
+              </ul>
+            </li>
+            <li class="work-position">
+              <h5>Estructuras de control&nbsp;&nbsp;&nbsp;<a href="https://github.com/MarioOsuna/2DAW_DWES/tree/main/1_Trimestre/EstructurasDeControl"><img class="logo" src="img/github.png" alt=""></a></h5>
+
+              <ul>
+                <li><a href="1_Trimestre/EstructurasDeControl/prueba.php">calendario1</a></li>
+                <li><a href="1_Trimestre/EstructurasDeControl/calendario.php">calendario2</a></li>
+                <li><a href="1_Trimestre/EstructurasDeControl/1.php">Ejercicio 1</a></li>
+                <li><a href="1_Trimestre/EstructurasDeControl/2.php">Ejercicio 2</a></li>
+                <li><a href="1_Trimestre/EstructurasDeControl/3.php">Ejercicio 3</a></li>
+                <li><a href="1_Trimestre/EstructurasDeControl/4.php">Ejercicio 4</a></li>
+              </ul>
+            </li>
+            <li class="work-position">
+              <h5>Estructuras&nbsp;&nbsp;&nbsp;<a href="https://github.com/MarioOsuna/2DAW_DWES/tree/main/1_Trimestre/Estructuras"><img class="logo" src="img/github.png" alt=""></a></h5>
+
+              <ul>
+                <li><a href="1_Trimestre/Estructuras/1.php">Ejercicio 1</a></li>
+                <li><a href="1_Trimestre/Estructuras/2.php">Ejercicio 2</a></li>
+                <li><a href="1_Trimestre/Estructuras/3.php">Ejercicio 3</a></li>
+                <!-- <li><a href="Estructuras/4.php">Ejercicio4</a></li> -->
+              </ul>
+
+            </li>
+            <li class="work-position">
+              <h5>Básicos&nbsp;&nbsp;&nbsp;<a href="https://github.com/MarioOsuna/2DAW_DWES/tree/main/1_Trimestre/Basicos"><img class="logo" src="img/github.png" alt=""></a></h5>
+
+              <ul>
+                <li><a href="1_Trimestre/Basicos/1.php">Ejercicio 1</a></li>
+                <li><a href="1_Trimestre/Basicos/2.php">Ejercicio 2</a></li>
+                <li><a href="1_Trimestre/Basicos/3.php">Ejercicio 3</a></li>
+                <li><a href="1_Trimestre/Basicos/4.php">Ejercicio 4</a></li>
+                <li><a href="1_Trimestre/Basicos/5.php">Ejercicio 5</a></li>
+              </ul>
+
+            </li>
+          </ul>
+          <div>
+            <h3 class="border-bottom-gray">PRÁCTICAS</h3>
+            <div class="project">
+              <h5>Examen 2019/2020</h5>
+              <div class="row">
+                <div class="col-3">
+                  <img id="profile-picture" src="img/cartas.jpg" alt="">
+                </div>
+                <div class="col">
+                  <p>Realizar un inicio de sesión, y un apartado con noticias y otro con un juego de cartas</p>
+                  <strong><a href="1_Trimestre/examen2019-2020/index.php">Examen 2019/2020</a></strong>
+                  <span class="badge badge-secondary">PHP</span>
+                  <span class="badge badge-secondary">HTML</span>
+                  <span class="badge badge-secondary">CSS</span>
+                  <a href="https://github.com/MarioOsuna/2DAW_DWES/tree/main/1_Trimestre/examen2019-2020"><img class="logo" src="img/github.png" alt=""></a>
+                </div>
+              </div>
+            </div>
+            <div class="project">
+              <h5>Agenda de contactos</h5>
+              <div class="row">
+                <div class="col-3">
+                  <img id="profile-picture" src="img/contactos.png" alt="">
+                </div>
+                <div class="col">
+                  <p>Agenda que almacena contactos</p>
+                  <strong><a href="1_Trimestre/Sesiones/agenda.php">Agenda de contactos</a></strong>
+                  <span class="badge badge-secondary">PHP</span>
+                  <span class="badge badge-secondary">HTML</span>
+                  <span class="badge badge-secondary">CSS</span>
+                  <a href="https://github.com/MarioOsuna/2DAW_DWES/blob/main/1_Trimestre/Sesiones/agenda.php"><img class="logo" src="img/github.png" alt=""></a>
+                </div>
+              </div>
+            </div>
+
+            <div class="project">
+              <h5>Práctica verbos irregulares (formularios)</h5>
+              <div class="row">
+                <div class="col-3">
+                  <img id="profile-picture" src="img/verbos.png" alt="">
+                </div>
+                <div class="col">
+                  <p>Práctica de verbos irregulares con distinta dificultad apareciendo diferentes campos para escirbir más verbos según la dificultad </p>
+                  <strong><a href="1_Trimestre/verbos/index.php">Test de verbos irregulares</a></strong>
+                  <span class="badge badge-secondary">PHP</span>
+                  <span class="badge badge-secondary">HTML</span>
+                  <span class="badge badge-secondary">CSS</span>
+                  <a href="https://github.com/MarioOsuna/2DAW_DWES/tree/main/1_Trimestre/verbos"><img class="logo" src="img/github.png" alt=""></a>
+                </div>
+              </div>
+            </div>
+            <div class="project">
+              <h5>Buscaminas (sesiones)</h5>
+              <div class="row">
+                <div class="col-3">
+                  <img id="profile-picture" src="img/buscaminas.jpg" alt="">
+                </div>
+                <div class="col">
+                  <p>Práctica buscaminas</p>
+                  <strong><a href="1_Trimestre/Buscaminas/buscaminas.php">Buscaminas</a></strong>
+                  <span class="badge badge-secondary">PHP</span>
+                  <span class="badge badge-secondary">HTML</span>
+                  <span class="badge badge-secondary">CSS</span>
+                  <a href="https://github.com/MarioOsuna/2DAW_DWES/tree/main/1_Trimestre/Buscaminas"><img class="logo" src="img/github.png" alt=""></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+      <div class="col-3">
+        <h3 class="border-bottom-gray">Skills & Tools</h3>
+        <h4>Backend</h4>
+        <ul>
+          <li>PHP</li>
+        </ul>
+        <h4>Frontend</h4>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+        </ul>
+        <h4>Frameworks</h4>
+        <ul>
+          <li>Visual Studio Code</li>
+        </ul>
+        <h3 class="border-bottom-gray">Languages</h3>
+        <ul>
+          <li>Spanish</li>
+          <li>English</li>
+        </ul>
+      </div>
+    </div>
+    <div id="resume-footer" class="row">
+      <div class="col">
+        Designed by MarioOsuna
+      </div>
+    </div>
+  </div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+</body>
+
+</html>
